0563f9fd40181e469dc04744fb8a3c67984e4bc6
godwillA33peo <godwillzulu51@gmail.com>
2025-11-25 12:23:32 +0000

    updating source files

--- app.R
+++ app.R
@@ -2,5 +2,5 @@ library(shiny)
 source("global.R", local = TRUE)
 source("ui/ui_main.R",     local = TRUE)
 source("server/server_main.R", local = TRUE)
-
+source("TeachBayes.r", local = TRUE)
 shinyApp(ui = ui, server = server)
