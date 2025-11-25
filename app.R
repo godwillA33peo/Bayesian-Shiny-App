42c10b5a738a3e02945472a895bc3992ea6066e0
godwillA33peo <godwillzulu51@gmail.com>
2025-11-25 23:13:41 +0000

    removing teachbayes for testing in shiny io

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 library(shiny)
+library(TeachBayes)
 source("global.R", local = TRUE)
 source("ui/ui_main.R",     local = TRUE)
 source("server/server_main.R", local = TRUE)
 source("TeachBayes.r", local = TRUE)
+
 shinyApp(ui = ui, server = server)
