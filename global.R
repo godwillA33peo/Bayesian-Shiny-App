fe322f5d889b2057f35bed1b1c39587cdaf75623
godwillA33peo <godwillzulu51@gmail.com>
2025-11-25 13:07:20 +0000

    Dependency updates

--- global.R
+++ global.R
@@ -1,9 +1,9 @@
 library(shiny)
 library(bslib)
-source("TeachBayes.r")
+library(TeachBayes)
+library(rsconnect)
 library(ggplot2)
 library(plotly)
 library(tidyr)
 library(dplyr)
 library(shinythemes)
-
